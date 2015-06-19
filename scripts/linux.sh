980b9938a2028eaaff6056734a53d3d3a8739c15
lindsey.smith@gmail.com <lindsey.smith@gmail.com>
2015-06-19 11:33:30 -0700

    Add /usr/local/bin to root shell so that puppet binaries are in the path

--- scripts/linux.sh
+++ scripts/linux.sh
@@ -1,4 +1,6 @@
 sudo service iptables stop
 sudo chkconfig iptables off
 cp /vagrant/lotsofnodes/mknodes.sh /home/vagrant
+sudo echo 'pathmunge /usr/local/bin' > /etc/profile.d/usr_local_bin.sh
+sudo chmod +x /etc/profile.d/usr_local_bin.sh
 
