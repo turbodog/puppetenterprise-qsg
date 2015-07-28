bb13de3c0178da0ec01a6ab68948ae475469566a
lindsey.smith@gmail.com <lindsey.smith@gmail.com>
2015-07-28 15:44:07 -0700

    Set agent hostname to agent so that certname jiggery pokery is no longer required

--- scripts/agent.sh
+++ scripts/agent.sh
@@ -1,6 +1,4 @@
 cp /vagrant/lotsofnodes/mknodes.sh /home/vagrant
-echo "curl -k https://master:8140/packages/current/install.bash | sudo bash -s agent:certname=agent" > /home/vagrant/installagent.sh
+sudo hostname agent
+echo "curl -k https://master:8140/packages/current/install.bash | sudo bash" > /home/vagrant/installagent.sh
 chmod +x /home/vagrant/installagent.sh
-#/home/vagrant/installagent.sh
-#sudo /usr/local/bin/puppet agent -t --certname agent
-#exit 0
