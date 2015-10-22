3e297ce2237f62c0cbeceaf5ab051cda3ba65039
lindsey.smith@gmail.com <lindsey.smith@gmail.com>
2015-10-22 10:07:26 -0700

    Pipe noise from disabling firewall on EL6 to /dev/null

--- scripts/linux.sh
+++ scripts/linux.sh
@@ -1,5 +1,12 @@
-sudo service iptables stop
-sudo chkconfig iptables off
+ELVER=`gawk -F' ' '{print $4}' /etc/redhat-release | head -c1`
+if [ "$ELVER" = "7" ]; then
+ systemctl stop firewalld
+ systemctl disable firewalld >& /dev/null
+else
+  sudo service iptables stop
+  sudo chkconfig iptables off
+fi
+
 cp /vagrant/lotsofnodes/mknodes.sh /home/vagrant
 sudo echo 'pathmunge /usr/local/bin' > /etc/profile.d/usr_local_bin.sh
 sudo chmod +x /etc/profile.d/usr_local_bin.sh
